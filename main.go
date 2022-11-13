1cce8687cf07e0fbae7fd55be83dbb205f7852e0
James Best <jim@justjibba.net>
2022-11-13 21:39:15 +0000

    Add create snippet route and only allow POST requests

--- main.go
+++ main.go
@@ -8,13 +8,46 @@ import (
 // Define a home handler which writes a byte slice containing
 // "Hello from snippet box"
 func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request url exactly matches "/".
+	// If it doesnt return 404
+	// IMPORTANT: We return from the handler. If we did not the handler
+	// would keep executing
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello"))
+}
+
+// snippetView handler
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a snippet"))
+}
+
+// snippetCreate
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// r.Method to check whether the request is using POST or not
+	if r.Method != http.MethodPost {
+		//Send 405 status code and message
+		w.Header().Set("Allow", http.MethodPost)
+		// w.WriteHeader(405)
+		// w.Write([]byte("Method not allowed"))
+
+		// this is a shortcut for the above
+		// we are passing w to http.Error that sends the reponse for us
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("snippet create"))
 }
 
 func main() {
 	// User the http.NewServerMux() to initialise a new router
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Use http.ListenAndServ() fun to start a new server
 	// take 2 params, address (:4000) and the servemux
---
+++ notes/notes.md
@@ -0,0 +1,15 @@
+# Notes
+
+## **fix path** and **subtree paths**
+
+- fix path dont end in a slash `/snippets/view` etc
+- subtree paths match when the _start_ of the path match. A bit like a wild card
+
+## Default serveMux()
+
+It is possible to not create a `NewServeMux` and just do `HandleFunc`. This creates a `DefaultServeMux` which is global. This is not recommended to prod applications as other 3rd party packages would also be able to add routes to your app. If they are compromised this would be bad
+
+## Writing headers
+
+- Its only possible to call `w.WriteHeader` once per reponse
+- If not called explicitly, the first `w.Write` will send 200 OK
