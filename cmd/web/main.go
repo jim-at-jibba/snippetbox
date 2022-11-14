28c435340d72b1a9d119f61c6e65a007c8e8c353
James Best <jim@justjibba.net>
2022-11-14 15:18:06 +0000

    Add ability to disable directory listings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
 	// User the http.NewServerMux() to initialise a new router
 	mux := http.NewServeMux()
+
+	// Create a file server to serve our static files from ./ui/static
+	// The path is relative to project root
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+
+	// use mux.Handle to register the file server as a handler
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
@@ -17,4 +26,31 @@ func main() {
 	log.Print("Starting server on :4000 🚀")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+// https://www.alexedwards.net/blog/disable-http-fileserver-directory-listings
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, _ := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
