e3735f463d39cc38c82c46f8827f5acf7fc0416b
James Best <jim@justjibba.net>
2022-11-18 05:56:52 +0000

    Add create snippet db code and wired it to the handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -73,5 +73,16 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("snippet create"))
+	title := "O snail"
+	content := "O snial, where are these snippets coming from?"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
