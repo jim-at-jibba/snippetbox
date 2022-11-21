9dad431157a4fc4899a99288d0a9b606b673002b
James Best <jim@justjibba.net>
2022-11-21 16:57:56 +0000

    Add better dynamic data error handling

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -38,12 +39,26 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 		return
 	}
 
-	// write out the  provided http status
-	w.WriteHeader(status)
+	// We are making the render a 2 step process
+	// Step 1: make a trial render by writing the template to a buffer, if it fails respond with an error
+	// Step 2: write the contents to our `http.ResponseWriter`
 
-	// Execute the template set and write the response
-	err := ts.ExecuteTemplate(w, "base", data)
+	// Initialise a new buffer
+	buf := new(bytes.Buffer)
+
+	// Write teh template to the buffer, instead of straight to the
+	// http.ResponseWriter, if there is an serverError
+	err := ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.serverError(w, err)
+		return
 	}
+
+	// If the template is written to the buffer without any errors, we are safe
+	// to write the HTTP status code
+	w.WriteHeader(status)
+
+	// Write the contents of the buffer to the http.ResponseWriter. Note this is another
+	// time where we pass our http.ResponseWriter to a function that takes an io.Writer
+	buf.WriteTo(w)
 }
