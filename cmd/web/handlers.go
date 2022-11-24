d3b359b0810a178f60fa7ce6dd9e3fffb878cdd5
James Best <jim@justjibba.net>
2022-11-24 16:40:46 +0000

    Update handler to accept incoming data and make snippet

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -118,9 +118,22 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	// 	return
 	// }
 
-	title := "O snail"
-	content := "O snial, where are these snippets coming from?"
-	expires := 7
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+
+	// Becaise values come from .Get as a string and we need a number we need to
+	// convert it
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
 	id, err := app.snippets.Insert(title, content, expires)
 
@@ -129,5 +142,5 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
